5700d295a66c9debaf2c8145ddca242229d2f7d5
Mohammed Azab <Mo7ammed3zab@outlook.com>
2025-01-07 17:42:05 +0100

    improved the CannotBeInterpolated RuntimeException

--- src/main/java/Exceptions/CannotBeInterpolated.java
+++ src/main/java/Exceptions/CannotBeInterpolated.java
@@ -4,4 +4,15 @@ public class CannotBeInterpolated extends RuntimeException {
     public CannotBeInterpolated(String message) {
         super(message);
     }
+
+    public CannotBeInterpolated(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public CannotBeInterpolated(Throwable cause) {
+        super(cause);
+    }
+
+    public CannotBeInterpolated() {
+        super("Can't be interpolated");
+    }
 }
